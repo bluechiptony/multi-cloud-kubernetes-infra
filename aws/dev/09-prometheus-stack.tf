843535fa97ffcf7b65210716d2353d7e11622103
Anthony Egwu <tony5egwu@gmail.com>
2023-12-05 21:23:09 +0100

    update helm release (#6)

--- aws/dev/09-prometheus-stack.tf
+++ aws/dev/09-prometheus-stack.tf
@@ -2,7 +2,7 @@ resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  version    = "4.5.2"  # Specify the desired chart version
+#   version    = "4.5.2"  # Specify the desired chart version
 
   namespace  = "kube-prometheus-stack"
   create_namespace = true
